697f405fee657ce1cb405ac4562d340ebe0d10b3
Andy Doan <andy@foundries.io>
2020-05-29 13:11:03 -0500

    Add callback server
    This lets us coordinate with aktualizr callbacks to kick off testing
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ fiotest/callbacks.py
@@ -0,0 +1,61 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from typing import Optional
+
+
+class AktualizrCallbackHandler:
+    def on_install_pre(self, current_target: str):
+        raise NotImplementedError()
+
+    def on_install_post(self, current_target: str, status: str):
+        raise NotImplementedError()
+
+
+class AktualizrCallbacks(SimpleHTTPRequestHandler):
+    callback: AktualizrCallbackHandler = AktualizrCallbackHandler()
+
+    def do_POST(self):
+        length = int(self.headers["content-length"])
+        buf = self.rfile.read(length)
+        parts = buf.decode().split(",")
+
+        msg: str = ""
+        current: str = ""
+        status: Optional[str] = None
+
+        try:
+            if len(parts) == 3:
+                msg, current, status = parts
+            elif len(parts) == 2:
+                msg, current = parts
+            else:
+                raise ValueError(buf)
+
+            if msg == "install-post":
+                assert status is not None  # for mypy
+                self.callback.on_install_post(current, status)
+            if msg == "install-pre":
+                self.callback.on_install_pre(current)
+            else:
+                self.log_message("Ignoring callback msg: %s", msg)
+
+        except Exception as e:
+            self.log_error("error: %r", e)
+            self.send_response(400)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(str(e).encode())
+            return
+
+        self.send_response(201)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"OK\n")
+
+
+class CallbackServer:
+    def __init__(self, handler: AktualizrCallbackHandler):
+        AktualizrCallbacks.callback = handler
+        self.httpd = HTTPServer(("", 8000), AktualizrCallbacks)
+
+    def run_forever(self):
+        self.httpd.serve_forever()
--- fiotest/main.py
+++ fiotest/main.py
@@ -1,8 +1,13 @@
 import logging
+import os
 import sys
 
 import yaml
 
+from fiotest.callbacks import (
+    AktualizrCallbackHandler,
+    CallbackServer,
+)
 from fiotest.runner import SpecRunner
 from fiotest.spec import TestSpec
 
@@ -10,10 +15,30 @@ logging.basicConfig(level="INFO", format="%(asctime)s %(levelname)s: %(message)s
 log = logging.getLogger()
 
 
+class Coordinator(AktualizrCallbackHandler):
+    def __init__(self, spec: TestSpec):
+        self.runner = None
+        self.spec = spec
+        if os.path.exists(SpecRunner.reboot_state):
+            self.runner = SpecRunner(self.spec)
+            self.runner.start()
+
+    def on_install_pre(self, current_target: str):
+        if self.runner:
+            log.info("New target about to be installed, stopping testing")
+            self.runner.stop()
+            self.runner = None
+
+    def on_install_post(self, current_target: str, status: str):
+        if status == "OK":
+            log.info("New target installed - kicking off testing")
+            self.runner = SpecRunner(self.spec)
+            self.runner.start()
+
+
 def main(spec: TestSpec):
-    runner = SpecRunner(spec)
-    runner.start()
-    runner.join()
+    cb_server = CallbackServer(Coordinator(spec))
+    cb_server.run_forever()
 
 
 if __name__ == "__main__":
