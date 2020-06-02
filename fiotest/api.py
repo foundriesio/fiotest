77b97f9eb936c010258229da3774be546f63bca5
Andy Doan <andy@foundries.io>
2020-06-02 16:47:43 -0500

    Move api code into library
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ fiotest/api.py
@@ -0,0 +1,95 @@
+import json
+import os
+import requests
+import sys
+from time import sleep
+
+
+def status(msg: str, prefix: str = "== "):
+    """Print a commonly formatted status message to stdout.
+       It also ensures the buffer is flushed and written immediately"""
+    sys.stdout.buffer.write(prefix.encode())
+    sys.stdout.buffer.write(b" ")
+    sys.stdout.buffer.write(msg.encode())
+    sys.stdout.buffer.write(b"\n")
+    sys.stdout.buffer.flush()
+
+
+def _requests_op(op, *args, **kwargs):
+    """A retriable requests helper."""
+    for i in (1, 2, 4, 8, 16):
+        r = op(*args, **kwargs)
+        if r.status_code in (200, 201):
+            break
+        else:
+            status("ERROR with %s - HTTP_%d: %s" % (r.url, r.status_code, r.text))
+            sleep(i)
+    return r
+
+
+def post(*args, **kwargs):
+    return _requests_op(requests.post, *args, **kwargs)
+
+
+def put(*args, **kwargs):
+    return _requests_op(requests.put, *args, **kwargs)
+
+
+class API:
+    def __init__(self, sota_dir: str, dryrun: bool):
+        self.dryrun = dryrun
+
+        cur_target = self.target_name(sota_dir)
+        self.url = self.test_url(sota_dir)
+        status("Test URL: " + self.url)
+
+        self.ca = os.path.join(sota_dir, "root.crt")
+        self.cert = (
+            os.path.join(sota_dir, "client.pem"),
+            os.path.join(sota_dir, "pkey.pem"),
+        )
+        self.headers = {"x-ats-target": cur_target}
+
+    def start_test(self, name: str) -> str:
+        data = {"name": name}
+        if not self.dryrun:
+            r = post(
+                self.url,
+                json=data,
+                headers=self.headers,
+                verify=self.ca,
+                cert=self.cert,
+            )
+            if r.status_code != 201:
+                sys.exit("Unable to start test: HTTP_%d: %s" % (r.status_code, r.text))
+            return r.text.strip()
+        return "DRYRUN"
+
+    def complete_test(self, test_id: str, data: dict) -> str:
+        if self.dryrun:
+            print(json.dumps(data, indent=2))
+        else:
+            r = put(self.url + "/" + test_id, json=data, verify=self.ca, cert=self.cert)
+            if r.status_code != 200:
+                sys.exit(
+                    "Unable to complete test: HTTP_%d: %s" % (r.status_code, r.text)
+                )
+
+    @staticmethod
+    def target_name(sota_dir: str) -> str:
+        with open(os.path.join(sota_dir, "current-target")) as f:
+            for line in f:
+                if line.startswith("TARGET_NAME"):
+                    k, v = line.split("=")
+                    return v.replace('"', "").strip()  # remove spaces and quotes
+        sys.exit("Unable to find current target")
+
+    @staticmethod
+    def test_url(sota_dir: str) -> str:
+        with open(os.path.join(sota_dir, "sota.toml")) as f:
+            for line in f:
+                if line.startswith("server ="):
+                    k, v = line.split("=")
+                    v = v.replace('"', "").strip()  # remove spaces and quotes
+                    return v + "/tests"
+        sys.exit("Unable to find server url")
