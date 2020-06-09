64e2fd515552183dbda2384bc8851d0d3f27adee
Andy Doan <andy@foundries.io>
2020-06-08 22:43:46 -0500

    aklite-callback: Add retry logic
    On a platform install we have:
    
     system reboot
     aktualizr-lite comes up, completes install, notifies callback
    
    Usually this will happen *before* docker gets the test container
    running. This change adds some retry logic to give it a chance to start
    running.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- aklite-callback.sh
+++ aklite-callback.sh
@@ -8,4 +8,17 @@ data="${MESSAGE},${CURRENT_TARGET}"
 if [ -n "$RESULT" ] ; then
 	data="${data},${RESULT}"
 fi
-wget -O- -q --post-data="${data}" http://localhost:8000/
+
+# After a platform install, the system will reboot and aktualizr-lite will
+# start before docker gets the container up and running. This adds some
+# back-off logic so that we don't miss aktualizr-lite's install-post message
+# that will trigger a new test run.
+for i in 1 2 4 8 16 ; do
+	if wget -O- -q --post-data="${data}" http://localhost:8000/ ; then
+		exit 0
+	fi
+	echo "Callback failure, retrying in $i seconds"
+	sleep $i
+done
+echo "Unable to notify callback"
+exit 1
