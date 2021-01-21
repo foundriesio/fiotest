85dd30067108d5e94b6b9ca287258c59c2d75c58
Andy Doan <andy@foundries.io>
2021-01-21 09:02:09 -0600

    aklite-callback: Detect when fiotest has been uninstalled
    Signed-off-by: Andy Doan <andy@foundries.io>

--- aklite-callback.sh
+++ aklite-callback.sh
@@ -4,6 +4,23 @@
 # It notifies the fiotest container about the callback so that it can
 # handle everything.
 
+# First make sure this app is still installed:
+# aktualizr-lite "uninstalls" a compose-app by basically running:
+#  docker-compose stop; rm -rf /var/sota/compose-apps/fiotest
+# we can detect we were deleted by looking for that directory:
+cd $(dirname $0)
+if [ ! -d ./compose-apps/fiotest ] ; then
+	if [ -f /etc/sota/conf.d/z-90-fiotest.toml ] ; then
+		echo "fiotest appears to be removed. disabling callback"
+		rm /etc/sota/conf.d/z-90-fiotest.toml
+	fi
+
+	# this gets tricky: we need to restart aklite. We don't want to do
+	# it when aklite is performing an update. To be safe we just exit
+	# now and eventually aklite will restart and stop calling us.
+	exit 0
+fi
+
 data="${MESSAGE},${CURRENT_TARGET}"
 if [ -n "$RESULT" ] ; then
 	data="${data},${RESULT}"
